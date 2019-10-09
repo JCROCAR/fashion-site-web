21213df0620d6656385eabe736fbf38ad442d31b
Juan Carlos Roca <juancarlos.rr97@gmail.com>
2019-10-09 11:53:48 -0600

    Todo el proyecto
    Soluciones qleys

---
+++ gulpfile.js
@@ -0,0 +1,68 @@
+"use strict";
+
+var gulp = require('gulp'),
+  concat = require('gulp-concat'),
+  uglify = require('gulp-uglify'),
+  rename = require('gulp-rename'),
+    sass = require('gulp-sass'),
+    maps = require('gulp-sourcemaps'),
+     del = require('del'),
+     autoprefixer = require('gulp-autoprefixer'),
+     browserSync = require('browser-sync').create(),
+     htmlreplace = require('gulp-html-replace'),
+     cssmin = require('gulp-cssmin');
+
+gulp.task("concatScripts", function() {
+    return gulp.src([
+        'assets/js/vendor/jquery-3.3.1.min.js',
+        'assets/js/vendor/popper.min.js',
+        'assets/js/vendor/bootstrap.min.js'
+        ])
+    .pipe(maps.init())
+    .pipe(concat('main.js'))
+    .pipe(maps.write('./'))
+    .pipe(gulp.dest('assets/js'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task("minifyScripts", ["concatScripts"], function() {
+  return gulp.src("assets/js/main.js")
+    .pipe(uglify())
+    .pipe(rename('main.min.js'))
+    .pipe(gulp.dest('docs/assets/js'));
+});
+
+gulp.task('compileSass', function() {
+  return gulp.src("assets/css/main.scss")
+      .pipe(maps.init())
+      .pipe(sass().on('error', sass.logError))
+      .pipe(autoprefixer())
+      .pipe(maps.write('./'))
+      .pipe(gulp.dest('assets/css'))
+      .pipe(browserSync.stream());
+});
+
+gulp.task("minifyCss", ["compileSass"], function() {
+  return gulp.src("assets/css/main.css")
+    .pipe(cssmin())
+    .pipe(rename('main.min.css'))
+    .pipe(gulp.dest('docs/assets/css'));
+});
+
+
+gulp.task('serve', function() {
+    browserSync.init({
+        server: './'
+    });
+});
+
+gulp.watch(['assets/css/main.scss'], ['compileSass']);
+gulp.watch(['assets/css/main.css'], ['minifyCss']);
+gulp.watch(['assets/js/vendor/jquery-3.3.1.min.js',
+'assets/js/vendor/popper.min.js',
+'assets/js/vendor/bootstrap.min.js'], ['concatScripts']);
+gulp.watch('./*.html').on('change', browserSync.reload);
+
+// Start server on default task
+
+gulp.task('default',['minifyScripts','minifyCss','serve']);
